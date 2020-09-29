842856ab8d799cfb916065809c477869f8d9f043
cartern206 <42358519+cartern206@users.noreply.github.com>
2020-09-28 21:13:59 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,101 +1,74 @@
-
-
+import time
+#import board
+#import neopixel
 import requests
 import json
 url = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"
-url2  = "https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams"
 response = json.loads(requests.get(url).text)
 
-
-# In[2]:
-
-
-#home team name
-print(response["events"][0]["competitions"][0]["competitors"][0]["team"]["shortDisplayName"])
-
-
-# In[3]:
-
-
-# Score response["events"][0]["competitions"][0]["competitors"][0]["score"]
-# team response["events"][0]["competitions"][0]["competitors"][0]["team"]["shortDisplayName"]
-# live print response["events"][0]["status"]["type"]["name"]
-# pretty print print(json.dumps(teams, indent=4, sort_keys=True))
-# shortDisplayName because of the R#*$%!N$
-
-
-# In[4]:
-
-
-team0s = json.loads(requests.get(url2).text)
-
-print (team0s)
-
-
-# In[45]:
-
-
+ 
+# Choose an open pin connected to the Data In of the NeoPixel strip, i.e. board.D18
+# NeoPixels must be connected to D10, D12, D18 or D21 to work.
+#pixel_pin = board.D18 
+# The number of NeoPixels
+num_pixels = 7
+ 
+# The order of the pixel colors - RGB or GRB. Some NeoPixels have red and green reversed!
+# For RGBW NeoPixels, simply change the ORDER to RGBW or GRBW.
+#ORDER = neopixel.GRB
+ 
+#pixels = neopixel.NeoPixel(
+ #   pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+#)
 teams = {
-"Cardinals":["255,0,0",0],
-"Falcons":["255,0,0",0],
-"Bills":["0,0,255",0],
-"Bears":["0,0,255",0],
-"Bengals":["255,150,20",0],
-"Browns":["255,150,20",0],
-"Cowboys":["0,0,255",0],
-"Broncos":["0,0,255",0],
-"Lions":["0,0,255",0],
-"Packers":["0,255,0",0],
-"Colts":["0,0,255",0],
-"Chiefs":["255,0,0",0],
-"Raiders":["255,0,255",0],
-"Chargers":["0,0,255",0],
-"Rams":["0,0,255",0],
-"Dolphins":["0,255,0",0],
-"Vikings":["75,0,130",0],
-"Patriots":["255,0,0",0],
-"Saints":["255,0,255",0],
-"Giants":["0,0,255",0],
-"Jets":["0,255,0",0],
-"Eagles":["0,255,0",0],
-"Steelers":["255,0,255",0],
-"49ers":["255,0,0",0],
-"Titans":["0,0,255",0],
-"Jaguars":["0,0,255",0],
-"Seahawks":["0,0,255",0],
-"Texans":["0,0,255",0],
-"Ravens":["75,0,130",0],
-"Panthers":["75,0,130",0],
-"Buccaneers":["75,0,130",0],   
-"Washington":["255,0,0",0]
+"Cardinals":[(255,0,0),0],
+"Falcons":[(255,0,0),0],
+"Bills":[(0,0,255),0],
+"Bears":[(0,0,255),0],
+"Bengals":[(255,150,20),0],
+"Browns":[(255,150,20),0],
+"Cowboys":[(0,0,255),0],
+"Broncos":[(0,0,255),0],
+"Lions":[(0,0,255),0],
+"Packers":[(0,255,0),0],
+"Colts":[(0,0,255),0],
+"Chiefs":[(255,0,0),0],
+"Raiders":[(255,0,255),0],
+"Chargers":[(0,255,0),0],
+"Rams":[(0,255,0),0],
+"Dolphins":[(0,255,0),0],
+"Vikings":[(75,0,130),0],
+"Patriots":[(255,0,0),0],
+"Saints":[(0,255,0),0],
+"Giants":[(0,0,255),0],
+"Jets":[(0,255,0),0],
+"Eagles":[(0,255,0),0],
+"Steelers":[(255,0,255),0],
+"49ers":[(255,0,0),0],
+"Titans":[(0,0,255),0],
+"Jaguars":[(0,0,255),0],
+"Seahawks":[(0,0,255),0],
+"Texans":[(0,0,255),0],
+"Ravens":[(75,0,130),0],
+"Panthers":[(0,0,255),0],
+"Buccaneers":[(255,0,0),0],   
+"Washington":[(255,0,0),0]
 }
 
-print(teams["Cardinals"])
 
-
-# In[51]:
-
-
-for song in response["events"]:   
-    #array 
-    print (teams[song["competitions"][0]["competitors"][1]["team"]["shortDisplayName"]][1])
-    # first team name
-    print (song["competitions"][0]["competitors"][1]["team"]["shortDisplayName"])
-    #first team score
-    print (song["competitions"][0]["competitors"][1]["score"])
-
-
-# In[50]:
-
-
-#compare score flash light and update score
- for song in response["events"]:   
+while True:
+    response = json.loads(requests.get(url).text)
+    for song in response["events"]:   
      #array 
-     if teams[song["competitions"][0]["competitors"][1]["team"]["shortDisplayName"]][1] -1 < int(song["competitions"][0]["competitors"][1]["score"]):
-         print ("touchdown")
-         teams[song["competitions"][0]["competitors"][1]["team"]["shortDisplayName"]][1] = int(song["competitions"][0]["competitors"][1]["score"])
-
-
-
-
-
+        x = 0
+        while x < 2:
+            if teams[song["competitions"][0]["competitors"][x]["team"]["shortDisplayName"]][1]  < int(song["competitions"][0]["competitors"][x]["score"]):
+                i=2
+                while i < 3:
+                    print (song["competitions"][0]["competitors"][x]["team"]["shortDisplayName"])
+                    print ("touch down")
+                    print (teams[song["competitions"][0]["competitors"][x]["team"]["shortDisplayName"]][1])
+                    print (int(song["competitions"][0]["competitors"][x]["score"]))
+                    i += 1
+                teams[song["competitions"][0]["competitors"][x]["team"]["shortDisplayName"]][1] = int(song["competitions"][0]["competitors"][x]["score"])
+            x+= 1
